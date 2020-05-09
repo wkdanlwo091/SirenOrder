1ac9ab467b3ee913772d6091609d83232eccbf54
minjae_Jang <wkdanlwo091@gmail.com>
2020-05-09 22:53:58 +0900

    sts4 boot start

---
+++ sirenorder/src/main/java/com/example/sirenorder/SirenorderApplication.java
@@ -0,0 +1,15 @@
+package com.example.sirenorder;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.example.sirenorder.mapper")
+public class SirenorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SirenorderApplication.class, args);
+	}
+
+}
---
+++ sirenorder/src/main/java/com/example/sirenorder/mapper/SirenOrderMapper.java
@@ -0,0 +1,5 @@
+package com.example.sirenorder.mapper;
+
+public class SirenOrderMapper {
+
+}
