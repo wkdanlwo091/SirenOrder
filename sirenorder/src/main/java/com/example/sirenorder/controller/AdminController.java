76db0341cfd3744a26a2f0ee5018a1d93575532b
minjae_Jang <wkdanlwo091@gmail.com>
2020-08-01 18:05:45 +0900

    usercheck page added

--- sirenorder/src/main/java/com/example/sirenorder/controller/AdminController.java
+++ sirenorder/src/main/java/com/example/sirenorder/controller/AdminController.java
@@ -1,27 +1,56 @@
 package com.example.sirenorder.controller;
 
+import java.util.ArrayList;
+
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.example.sirenorder.frame.Biz;
+import com.example.sirenorder.vo.UserVO;
 
 @Controller
 public class AdminController {
-	
+	@Resource(name = "userbiz")
+	Biz<String, UserVO> userbiz;
+
 	@RequestMapping(value = "/adminmain.html", method = RequestMethod.GET)
 	public ModelAndView login(HttpServletRequest request) {
 		ModelAndView model = new ModelAndView();
 
 		
 		
-
+		model.addObject("search", "clicked");
 		model.addObject("usercheck", "clicked");
 		model.setViewName("thymeleaf/adminmain");
 
 		return model;// id 없다.
 	}
-
+	
+	@RequestMapping(value = "/usercheck.html", method = RequestMethod.POST)
+	public ModelAndView usercheck(HttpServletRequest request,
+			@RequestParam(required = false) String users_id) {
+		ModelAndView model = new ModelAndView();
+		if(users_id != null) {
+			ArrayList<UserVO> userVO = new ArrayList<UserVO>();
+			userVO.add(userbiz.get(users_id));
+			model.addObject("userVOs", userVO);
+			model.addObject("usercheck", "clicked");
+			model.setViewName("thymeleaf/adminmain");
+		}else {
+			System.out.println("all users");
+			ArrayList<UserVO> userVO = userbiz.get();
+			System.out.println(userVO.get(0));
+			//모든 user 가져오기 페이지네이션은 추후에 적용하겠다. owner을 클릭하면 owner에게 chain_name과 store_name을 할당 할 수 있다.
+			model.addObject("userVOs", userVO);
+			model.addObject("usercheck", "clicked");
+			model.setViewName("thymeleaf/adminmain");
+		}
+		return model;// id 없다.
+	}
 }
