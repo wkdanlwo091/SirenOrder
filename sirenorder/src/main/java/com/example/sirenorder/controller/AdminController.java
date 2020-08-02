cd37e3cdcd95d2fc852a984a6ec5f5bc02ce4eec
minjae_Jang <wkdanlwo091@gmail.com>
2020-08-02 22:24:56 +0900

    usercheck setting owner info working on it

--- sirenorder/src/main/java/com/example/sirenorder/controller/AdminController.java
+++ sirenorder/src/main/java/com/example/sirenorder/controller/AdminController.java
@@ -23,56 +23,71 @@ public class AdminController {
 	public ModelAndView login(HttpServletRequest request) {
 		ModelAndView model = new ModelAndView();
 
-		
-		
 		model.addObject("search", "clicked");
 		model.addObject("usercheck", "clicked");
 		model.setViewName("thymeleaf/adminmain");
 
 		return model;// id 없다.
 	}
-	
+
 	@RequestMapping(value = "/usercheck.html", method = RequestMethod.GET)
-	public ModelAndView usercheck1(HttpServletRequest request,
-			@RequestParam(required = false) String users_id) {
+	public ModelAndView usercheck1(HttpServletRequest request, @RequestParam(required = false) String users_id) {
 		ModelAndView model = new ModelAndView();
-		if(users_id != null) {
+		if (users_id != null) {
 			ArrayList<UserVO> userVO = new ArrayList<UserVO>();
 			userVO.add(userbiz.get(users_id));
 			model.addObject("userVOs", userVO);
 			model.addObject("usercheck", "clicked");
 			model.setViewName("thymeleaf/adminmain");
-		}else {
+		} else {
 			System.out.println("all users");
 			ArrayList<UserVO> userVO = userbiz.get();
 			System.out.println(userVO.get(0));
-			//모든 user 가져오기 페이지네이션은 추후에 적용하겠다. owner을 클릭하면 owner에게 chain_name과 store_name을 할당 할 수 있다.
+			// 모든 user 가져오기 페이지네이션은 추후에 적용하겠다. owner을 클릭하면 owner에게 chain_name과 store_name을
+			// 할당 할 수 있다.
 			model.addObject("userVOs", userVO);
 			model.addObject("usercheck", "clicked");
 			model.setViewName("thymeleaf/adminmain");
 		}
 		return model;// id 없다.
 	}
-	
+
 	@RequestMapping(value = "/usercheck.html", method = RequestMethod.POST)
-	public ModelAndView usercheck(HttpServletRequest request,
-			@RequestParam(required = false) String users_id) {
+	public ModelAndView usercheck(HttpServletRequest request, @RequestParam(required = false) String users_id) {
 		ModelAndView model = new ModelAndView();
-		if(users_id != null) {
+		if (users_id != null) {
 			ArrayList<UserVO> userVO = new ArrayList<UserVO>();
 			userVO.add(userbiz.get(users_id));
 			model.addObject("userVOs", userVO);
 			model.addObject("usercheck", "clicked");
 			model.setViewName("thymeleaf/adminmain");
-		}else {
+		} else {
 			System.out.println("all users");
 			ArrayList<UserVO> userVO = userbiz.get();
-			System.out.println(userVO.get(0));
-			//모든 user 가져오기 페이지네이션은 추후에 적용하겠다. owner을 클릭하면 owner에게 chain_name과 store_name을 할당 할 수 있다.
+			// 모든 user 가져오기 페이지네이션은 추후에 적용하겠다. owner을 클릭하면 owner에게 chain_name과 store_name을
+			// 할당 할 수 있다.
 			model.addObject("userVOs", userVO);
 			model.addObject("usercheck", "clicked");
 			model.setViewName("thymeleaf/adminmain");
 		}
 		return model;// id 없다.
 	}
+
+	@RequestMapping(value = "/updateownerinfo", method = RequestMethod.POST)
+	public ModelAndView updateownerinfo(HttpServletRequest request,
+			@RequestParam(required = false) String users_id, 
+			@RequestParam(required = false) String chain_name, 
+			@RequestParam(required = false) String store_name
+			) {
+		ModelAndView model = new ModelAndView();
+		
+		userbiz.get(users_id);
+		//여기서 user의 store_name 업데이트 하고 store_name 
+		
+		
+		model.addObject("usercheck", "clicked");
+		model.setViewName("thymeleaf/adminmain");
+		return model;// id 없다.
+	}
+
 }
